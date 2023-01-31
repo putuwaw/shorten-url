88791f415d78def70e1d3cab957209cf9d8ac9cd
Putu Widyantara <putuwaw973@gmail.com>
2023-02-01 00:03:16 +0800

    Add modules to handle URL shortening

--- modules/modules.py
+++ modules/modules.py
@@ -1,10 +1,43 @@
-def hello():
-    return "Hello, World!"
+import random
+from models.url_list import UrlList
 
-def content():
-    return '''
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            '''
+
+def insert_to_database(db, url, shortUrl):
+    new = UrlList(
+        url=url,
+        url_short=shortUrl
+    )
+    db.session.add(new)
+    db.session.commit()
+
+
+def get_string_length():
+    count = UrlList.query.count()
+    return (count // (50 ** 4)) + 4
+
+
+def random_string(length):
+    lower = 'abcdefghijklmnopqrstuvwxyz'
+    upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    digits = '0123456789'
+
+    shortUrl = ''
+
+    isValid = False
+    while not isValid:
+        isValid = True
+        shortUrl = ''.join(random.choice(lower + upper + digits)
+                           for i in range(length))
+        count = UrlList.query.filter(UrlList.url_short == shortUrl).count()
+        if count > 0:
+            isValid = False
+
+    return shortUrl
+
+
+def check_custom_url(customUrl):
+    count = UrlList.query.filter(UrlList.url_short == customUrl).count()
+    if count > 0:
+        return "used"
+    else:
+        return customUrl
