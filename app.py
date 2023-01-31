d2f4025b3fcf7a66ec24f7fb6f420766f90db80f
Putu Widyantara <putuwaw973@gmail.com>
2023-01-31 23:41:51 +0800

    Configure app with database

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from handlers.routes import configure_routes
+from utils.db import db
+from utils.env import SQLALCHEMY_DATABASE_URI
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+db.init_app(app)
 
 configure_routes(app)
 
