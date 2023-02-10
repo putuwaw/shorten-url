55f4b14357a518737a8dc46d68e3ecbaf1d28a32
Putu Widyantara <putuwaw973@gmail.com>
2023-02-10 14:57:32 +0800

    Initialize cors for the app

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from handlers.routes import configure_routes
 from utils.db import db
 from utils.env import SQLALCHEMY_DATABASE_URI
 
 app = Flask(__name__)
+CORS(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 db.init_app(app)
 
