acc808db607e7beea40c0124cc1a79bbd81397da
Putu Widyantara <putuwaw973@gmail.com>
2023-02-13 12:40:24 +0800

    Add function to check valid url dan custom url

--- modules/modules.py
+++ modules/modules.py
@@ -1,4 +1,5 @@
 import random
+import re
 from models.url_list import UrlList
 
 
@@ -41,3 +42,19 @@ def check_custom_url(customUrl):
         return "used"
     else:
         return customUrl
+
+def is_valid_url(url):
+    pattern = re.compile(
+        r'^(?:http|ftp)s?://'  # http:// or https://
+        # domain...
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
+        r'localhost|'  # localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+        r'(?::\d+)?'  # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    return re.match(pattern, url) is not None
+
+
+def is_valid_custom(custom):
+    pattern = r'^[\w-]+$'
+    return re.match(pattern, custom)
